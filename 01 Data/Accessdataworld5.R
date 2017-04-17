34b63095057c1c79f071bd3d1514b724d6994e16
ASUS\Franny <frannyfang@gmail.com>
2017-04-17 17:24:40 -0500

    no message

---
+++ 01 Data/Accessdataworld5.R
@@ -0,0 +1,10 @@
+require(data.world)
+conn <- data.world()
+globalshipments <- query(conn, dataset="jlee/s-17-dv-project-5", type = "sql",query="SELECT * FROM `globalshipments.csv/globalshipments` LIMIT 10")
+
+require(data.world)
+conn <- data.world()
+census <- query(conn, dataset = "jlee/s-17-dv-project-5", type = "sql", query = "SELECT * FROM `census-pop-sex.csv/census-pop-sex` LIMIT 10")
+
+View(globalshipments)
+View(census)
